12fd941241cd759eb2d30aea58fb153625a3345a
Deukhwa <deukhwajeong@gmail.com>
2022-04-06 16:26:49 -0400

    add expenses components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import Expenses from './components/Expenses'
+
 function App() {
+  const expenses = [
+    {
+      title: 'Car Insurnace',
+      amount: 294.65,
+      date: new Date(2022, 3, 5),
+    },
+    {
+      title: 'Toilet paper',
+      amount: 24.22,
+      date: new Date(2022, 2, 2),
+    },
+    {
+      title: 'Laptop',
+      amount: 904.65,
+      date: new Date(2022, 3, 2),
+    },
+    {
+      title: 'iPhone',
+      amount: 1114.65,
+      date: new Date(2022, 3, 5),
+    },
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
